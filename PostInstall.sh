c4ea9cf7e1aea95bb54c0a8f7b587f3e3f61f73f
Davy Vandamme <DVA@Acertys.com>
2018-07-02 13:47:12 +0200

    another one

--- PostInstall.sh
+++ PostInstall.sh
@@ -22,3 +22,4 @@ curl -s http://www.webmin.com/jcameron-key.asc | apt-key add -
 echo "deb http://download.webmin.com/download/repository sarge contrib" | tee /etc/apt/sources.list.d/webmin.list
 apt-get update && apt-get -y install webmin
 
+# More to come ...
